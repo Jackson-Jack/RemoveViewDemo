2973521d0d961c01c4895d1dae62837000f904c8
DQB <dqb@jasons-macbook-pro.local>
2018-07-23 19:19:35 +0800

    Initial Commit

---
+++ removeFromSuperViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  removeFromSuperViewDemo
+//
+//  Created by DQB on 2018/7/23.
+//  Copyright © 2018年 QBTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
